7561d5e04122e9ff456b07b7f4c0a3654170520f
Praveen Kumar Pendyala <praveendath92@gmail.com>
2020-07-31 19:23:34 +0100

    Deleting backups older than 10 days

--- host-scripts/backup-config.sh
+++ host-scripts/backup-config.sh
@@ -2,10 +2,14 @@
 # Creates a backup of the config directory
 # Set this as a cron job and preferably sync the backup directory to cloud / reliable server
 TIME=`date +%b-%d-%y`
-FILENAME=backup-$TIME.tar.gz
+FILENAME=HA-Config-Backup-$TIME.tar.gz
 SRCDIR=$(dirname "$0"})/../config
 DESDIR=$(dirname "$0"})/../backups
 BACKUPNAME="$DESDIR/$FILENAME"
 mkdir -p $DESDIR
 tar -cpzf $BACKUPNAME $SRCDIR
 echo "Backed up $SRCDIR to $BACKUPNAME"
+
+# Delete backups older than 10 days
+find $DESDIR -name 'HA-Config-Backup-*.tar.gz' -mtime +10 -exec rm {}\;
+echo "Deleted backups older than 10 days"
