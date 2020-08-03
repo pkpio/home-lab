12e5f03e8cf591713a7d07edf5a2f547443d405f
Praveen Kumar Pendyala <praveen.pendyala@microsoft.com>
2020-08-03 23:47:19 +0100

    Create archive everyday at 2 am

---
+++ docker-volume/ofelia/backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Creates an archive of docker-volume each day at docker-volume/rclone/data
+# docker-volume/rclone/data itself won't be part of this archive
+
+TIME=`date +%b-%d-%y`
+ARCHIVE_FILENAME=docker-volume-$TIME.tar.gz
+
+SOURCE_DIR=/docker-volume
+DESTINATION_DIR=/docker-volume/rclone/data
+ARCHIVE_FILEPATH="$DESTINATION_DIR/$ARCHIVE_FILENAME"
+
+tar -cpz --exclude='/docker-volume/rclone/data' -f $ARCHIVE_FILEPATH $SOURCE_DIR
+echo "Backed up $ARCHIVE_FILEPATH to $SOURCE_DIR"
+
+# Delete backups older than 10 days
+find $DESTINATION_DIR -name 'docker-volume-*.tar.gz' -mtime +10 -exec rm {}\;
+echo "Deleted backups older than 10 days"
