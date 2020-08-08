bd8ccdb8fda999896113986709f2a570d459c2b7
Praveen Kumar Pendyala <praveen.pendyala@microsoft.com>
2020-08-08 01:00:16 +0100

    Merged contextual parameters updates into a single script

---
+++ docker-volume/home-assistant/python_scripts/update_contextual_params.py
@@ -0,0 +1,158 @@
+# Updates the initial brightness value of the lights based on time of the day
+brightnessPoints = {
+	"00:30": {
+		"startBrightness": 120,
+		"finalBrightness": 180,
+		"transitionTime": 45,
+		"color": [255, 255, 255]
+	},
+	"08:00": {
+		"startBrightness": 140,
+		"finalBrightness": 200,
+		"transitionTime": 15,
+		"color": [255, 255, 255]
+	},
+	"17:00": {
+		"startBrightness": 170,
+		"finalBrightness": 220,
+		"transitionTime": 10,
+		"color": [255, 255, 255]
+	},
+	"19:00": {
+		"startBrightness": 190,
+		"finalBrightness": 230,
+		"transitionTime": 10,
+		"color": [255, 255, 255]
+	},
+	"22:30": {
+		"startBrightness": 210,
+		"finalBrightness": 255,
+		"transitionTime": 15,
+		"color": [255, 255, 255]
+	},
+	"23:30": {
+		"startBrightness": 180,
+		"finalBrightness": 230,
+		"transitionTime": 20,
+		"color": [255, 255, 255]
+	}
+}
+
+# Based on minute of the day
+minutePoints = []
+for timePoint, contextData in brightnessPoints.items():
+	hours, minutes = timePoint.split(':')
+	minuteOfDay = int(hours) * 60 + int(minutes)
+	minutePoints.append({
+		"minuteOfDay": minuteOfDay,
+		"contextData": contextData
+	})
+sortedPoints = sorted(minutePoints, key = lambda point: point['minuteOfDay'])
+
+# Find the minute of day right now
+nowHour = int(data.get("hour"))
+nowMinute = int(data.get("minute"))
+minuteOfDay = nowHour * 60 + nowMinute
+
+leftIndex, rightIndex = -1, -1
+
+# Find leftIndex in the loop
+for index in range(len(sortedPoints)):
+	if sortedPoints[index]['minuteOfDay'] >= minuteOfDay:
+		rightIndex = index
+		break
+
+# If point found
+rightIndex = 0 if rightIndex == -1 else rightIndex
+leftIndex = rightIndex - 1 if rightIndex != 0 else len(sortedPoints) - 1
+
+leftRatio, rightRatio = 0.0, 0.0
+
+if sortedPoints[rightIndex]['minuteOfDay'] > sortedPoints[leftIndex]['minuteOfDay']:
+	# both points fall on the same day - simple scenario
+	pointDistance = sortedPoints[rightIndex]['minuteOfDay'] - sortedPoints[leftIndex]['minuteOfDay']
+	leftRatio = (minuteOfDay - sortedPoints[leftIndex]['minuteOfDay']) / pointDistance
+	rightRatio = 1.0 - leftRatio
+else:
+	# points are on either side of the day
+	pointDistance = 1440 - sortedPoints[leftIndex]['minuteOfDay'] + sortedPoints[rightIndex]['minuteOfDay']
+	if minuteOfDay <= sortedPoints[rightIndex]['minuteOfDay']:
+		# current time is in the day of right point
+		rightRatio = (sortedPoints[rightIndex]['minuteOfDay'] - minuteOfDay) / pointDistance
+		leftRatio = 1.0 - rightRatio
+	else:
+		# current time is in the day of left point
+		leftRatio = (minuteOfDay - sortedPoints[leftIndex]['minuteOfDay']) / pointDistance
+		rightRatio = 1.0 - leftRatio
+
+leftWeight, rightWeight = (1 - leftRatio), (1 - rightRatio)
+
+weightedIntVal = lambda key : int(sortedPoints[leftIndex]['contextData'][key] * leftWeight + sortedPoints[rightIndex]['contextData'][key] * rightWeight)
+weightedColorVal = lambda index : int(sortedPoints[leftIndex]['contextData']['color'][index] * leftWeight + sortedPoints[rightIndex]['contextData']['color'][index] * rightWeight)
+
+
+#### Set the start brightness ####
+hass.services.call(
+	"input_number", 
+	"set_value", 
+	{
+		"entity_id": "input_number.contextual_start_brightness", 
+		"value": weightedIntVal('startBrightness')
+	}, 
+	False
+)
+
+#### Set the final brightness ####
+hass.services.call(
+	"input_number", 
+	"set_value", 
+	{
+		"entity_id": "input_number.contextual_final_brightness", 
+		"value": weightedIntVal('finalBrightness')
+	}, 
+	False
+)
+#### Set the transition time ####
+hass.services.call(
+	"input_number", 
+	"set_value", 
+	{
+		"entity_id": "input_number.contextual_transition_time", 
+		"value": weightedIntVal('transitionTime')
+	}, 
+	False
+)
+
+
+#### Set the color - red ####
+hass.services.call(
+	"input_number", 
+	"set_value", 
+	{
+		"entity_id": "input_number.contextual_color_red", 
+		"value": weightedColorVal(0)
+	}, 
+	False
+)
+
+#### Set the color - green ####
+hass.services.call(
+	"input_number", 
+	"set_value", 
+	{
+		"entity_id": "input_number.contextual_color_green", 
+		"value": weightedColorVal(1)
+	}, 
+	False
+)
+
+#### Set the color - blue ####
+hass.services.call(
+	"input_number", 
+	"set_value", 
+	{
+		"entity_id": "input_number.contextual_color_blue", 
+		"value": weightedColorVal(2)
+	}, 
+	False
+)
