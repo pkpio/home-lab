645cd0940923f8d9da8eeaebc7f8758d5af02b62
Praveen Kumar Pendyala <praveen.pendyala@microsoft.com>
2020-08-17 12:47:45 +0100

    Updated docker service names

--- rclone/script/backup.sh
+++ rclone/script/backup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-# Creates an archive of docker-volume each day at rclone/data
-# rclone/data itself won't be part of this archive
+# Creates an archive of docker-volume each day at rclone/backups
+# rclone/backups itself won't be part of this archive
 
 TIME=`date +%b-%d-%y`
 ARCHIVE_FILENAME=all-containers-data-$TIME.tar.gz
 
 SOURCE_DIR=/all-containers-data
-DESTINATION_DIR=/all-containers-data/rclone/data
+DESTINATION_DIR=/backups
 ARCHIVE_FILEPATH="$DESTINATION_DIR/$ARCHIVE_FILENAME"
 
-tar --exclude='**/rclone/data/*' -cpzf $ARCHIVE_FILEPATH $SOURCE_DIR
+tar --exclude='**/rclone/backups*' -cpzf $ARCHIVE_FILEPATH $SOURCE_DIR
 echo "Backed up $ARCHIVE_FILEPATH to $SOURCE_DIR"
 
 # Delete backups older than 10 days
