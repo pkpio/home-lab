d24c1b48493eecc207a085ce3a402775bed10847
Praveen Kumar Pendyala <praveen.pendyala@microsoft.com>
2020-09-10 17:48:35 +0100

    Updated plants

--- home-assistant/config/python_scripts/plant_problems.py
+++ home-assistant/config/python_scripts/plant_problems.py
@@ -4,85 +4,73 @@
 # Make a list of all plants that need to be water and fertilized and combine into readable lists.
 #
 # Creates:
-# sensor.water_plants_number
-# sensor.fertilize_plants_number
-# sensor.plant_problems
+# sensor.plants_water_low
+# sensor.plants_fertilizer_low
+# sensor.plants_battery_low
+# sensor.plants_problems
 #
 #  http://www.diyfuturism.com/making-houseplants-talk
 
-problemPlants = 0
-allproblemPlants = []
-waterPlants = []
-numberWater = 0
-fertilizePlants = []
-numberFertilize = 0
-deadBatteries = []
-numberdeadBatteries = 0
+allproblemPlants = set()
+waterPlants = set()
+fertilizePlants = set()
+lowBatteryPlants = set()
 whichIcon = "mdi:help-circle-outline"
 
 for entity_id in hass.states.entity_ids('plant'):
   state = hass.states.get(entity_id)
   if state.state == 'problem':
-    problemPlants = problemPlants + 1
-    allproblemPlants.append(state.name)
     problem = state.attributes.get('problem') or 'none'
     if "conductivity low" in problem:
       fertilizePlants.append(state.name)
-      numberFertilize = numberFertilize + 1
     if "moisture low" in problem:
       waterPlants.append(state.name)
-      numberWater = numberWater + 1
     if "battery low" in problem:
-      deadBatteries.append(state.name)
-      numberdeadBatteries = numberdeadBatteries + 1
+      lowBatteryPlants.append(state.name)
+
+# Make a list of all problems
+allproblemPlants.extend(fertilizePlants)
+allproblemPlants.extend(waterPlants)
+allproblemPlants.extend(lowBatteryPlants)
 
 # Set icon
-if problemPlants > 0:
+if allproblemPlants:
   whichIcon = "mdi:alert-circle-outline"
 else:
   whichIcon = "mdi:check-circle-outline"
 
 # Set states
-hass.states.set('sensor.plant_problems', problemPlants, {
+hass.states.set('sensor.plants_problems', len(allproblemPlants), {
     'unit_of_measurement': 'plants',
     'friendly_name': 'Problem Plants',
     'icon': whichIcon,
     'problem_plants': allproblemPlants,
     'water': waterPlants,
-    'water_number': numberWater,
+    'water_number': len(waterPlants),
     'fertilize': fertilizePlants,
-    'fertilize_number': numberFertilize,
-    'battery_change': deadBatteries,
-    'battery_number': numberdeadBatteries
+    'fertilize_number': len(fertilizePlants),
+    'battery_low': lowBatteryPlants,
+    'battery_low_number': len(lowBatteryPlants)
 })
 
-hass.states.set('sensor.water_plants_number', numberWater, {
+hass.states.set('sensor.plants_water_low', len(waterPlants), {
     'unit_of_measurement': 'plants',
-    'friendly_name': 'Water Plants Number',
-    'icon': 'mdi:water'
-})
-
-waterplantsList = ', '.join(waterPlants)
-if waterplantsList == "":
-  waterplantsList = "None"
-hass.states.set('sensor.water_plants_friendly', waterplantsList, {
-    'friendly_name': 'Water Plants',
+    'friendly_name': 'Water plants count',
     'icon': 'mdi:water',
-    'water': waterPlants
+    'plants': waterPlants
 })
 
-hass.states.set('sensor.fertilize_plants_number', numberFertilize, {
+hass.states.set('sensor.plants_fertilizer_low', len(fertilizePlants), {
     'unit_of_measurement': 'plants',
-    'friendly_name': 'Fertilize Plants Number',
+    'friendly_name': 'Fertilize plants count',
     'icon': 'mdi:emoticon-poop',
-    'fertilize': fertilizePlants
+    'plants': fertilizePlants
 })
 
-fertilizeplantsList = ', '.join(fertilizePlants)
-if waterplantsList == "":
-  waterplantsList = "None"
-hass.states.set('sensor.fertilize_plants_friendly', fertilizeplantsList, {
+hass.states.set('sensor.plants_battery_low', len(fertilizePlants), {
     'unit_of_measurement': 'plants',
-    'friendly_name': 'Fertilize Plants',
-    'icon': 'mdi:emoticon-poop'
+    'friendly_name': 'Battery low plants',
+    'icon': 'mdi:battery-alert',
+    'plants': lowBatteryPlants
 })
+
